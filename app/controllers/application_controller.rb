41f4d9bc0c9d96c084b4ae4ba1c78207822b9678
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-15 16:49:09 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,7 +24,7 @@ class ApplicationController < Sinatra::Base
     erb :
   end
 
-  delete '/recipes/:id' do
+  delete '/recipes/:id/delete' do
     @recipe = Recipe.find_by_id(params[:id])
     erb :
   end
